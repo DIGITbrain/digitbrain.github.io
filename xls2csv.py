30e196295e6138594d67b6488a943d40bcd17574
jaydesl <35102795+jaydesl@users.noreply.github.com>
2022-01-10 15:18:00 +0000

    ci: generate header column

--- xls2csv.py
+++ xls2csv.py
@@ -4,6 +4,6 @@ excel = "metadata.xlsx"
 sheets = pd.read_excel(excel, sheet_name=None)
 
 for sheet_name in sheets.keys():
-    sheet = pd.read_excel(excel, sheet_name=sheet_name, skiprows=[0], usecols="B:G")
+    sheet = pd.read_excel(excel, sheet_name=sheet_name, skiprows=[0], usecols="A:G")
     sheet_name = sheet_name.lower().replace(" ", "_")
     sheet.to_csv(f"assets/{sheet_name}.csv", index=False)

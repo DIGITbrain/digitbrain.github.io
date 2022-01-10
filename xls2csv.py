99a903f5912b7396b59d9d220aaae347b8acd60d
jaydesl <35102795+jaydesl@users.noreply.github.com>
2022-01-10 15:23:48 +0000

    ci: output generate step

--- xls2csv.py
+++ xls2csv.py
@@ -6,4 +6,5 @@ sheets = pd.read_excel(excel, sheet_name=None)
 for sheet_name in sheets.keys():
     sheet = pd.read_excel(excel, sheet_name=sheet_name, skiprows=[0], usecols="A:G")
     sheet_name = sheet_name.lower().replace(" ", "_")
-    sheet.to_csv(f"assets/{sheet_name}.csv", index=False)
+    sheet.to_csv(f"assets/{sheet_name}.csv", index=False)    
+    print(f"Generating {sheet_name}.csv...")

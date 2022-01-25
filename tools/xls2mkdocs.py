0ea7e9e2fa99253c82afda5e1c89ab21760c057e
jaydesl <jay.deslauriers@gmail.com>
2022-01-25 10:29:20 +0000

    fix: avoid dupe keys with list

--- tools/xls2mkdocs.py
+++ tools/xls2mkdocs.py
@@ -10,13 +10,14 @@ for sheet_name in sheets.keys():
         excel, sheet_name=sheet_name, skiprows=[0], usecols="A:G"
     )
 
-    sheet_dictionary = {}
+    sheet_list = []
     for i, subkey in enumerate(sheet["Subkey"]):
 
         # Use a subkey if there is one, if NaN then skip this cell
-        field = subkey if subkey == subkey else sheet["Key"][i]
+        subkey = subkey if all([subkey, subkey == subkey]) else None
+        field = subkey if subkey else sheet["Key"][i]
         if isinstance(field, float):
-            sheet_dictionary[sheet["Concept"][i]] = None
+            sheet_list.append({"concept": sheet["Concept"][i]})
             continue
 
         # Discard NaN in example values and comments
@@ -29,18 +30,21 @@ for sheet_name in sheets.keys():
         )
 
         # Store fields data
-        sheet_dictionary[field] = (
-            sheet["Condition"][i],
-            comment,
-            sheet["Type"][i],
-            example,
-        )
+        sheet_list.append({
+            "concept": None,
+            "key": field,
+            "required": sheet["Condition"][i],
+            "comment": comment,
+            "type": sheet["Type"][i],
+            "example": example,
+            "issubkey": subkey,
+        })
 
     # Fill the template
     jinja_loader = jinja2.FileSystemLoader(searchpath="tools/jinja_templates")
     jinja_env = jinja2.Environment(loader=jinja_loader)
     template = jinja_env.get_template("docspage.md.j2")
-    rendered = template.render(sheet_name=sheet_name, fields=sheet_dictionary)
+    rendered = template.render(sheet_name=sheet_name, fields=sheet_list)
     file_name = sheet_name.lower().replace(" ", "_")
     with open(f"{SPECS_DIR}/{file_name}.md", "w") as file:
         file.write(rendered)

211e9323b6f98a295f8666d7ac6dec6acc39d75a
jaydesl <jay.deslauriers@gmail.com>
2022-02-11 17:37:41 +0000

    feat(xls2...): add link between table and list views

--- tools/xls2md.py
+++ tools/xls2md.py
@@ -8,7 +8,7 @@ hide_edits = """
   .md-content__button {
     display: none;
   }
-</style>
+</style>\n
 """
 
 
@@ -17,6 +17,10 @@ for tab in sheets.keys():
     sheet_name = tab.lower().replace(" ", "_")
     sheet.fillna("", inplace=True)
     table = sheet.to_markdown(index=False)
+    details_link = (
+      "**This information is also available in** "
+      f"**[list format](/attributes/{sheet_name}/)**\n\n"
+    )
     with open(f"docs/tables/{sheet_name}.md", "w") as file:
-        file.write(hide_edits + "\n" + table)
+        file.write(hide_edits + details_link + table)
     print(f"Converted to {sheet_name}.md...")

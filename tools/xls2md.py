41b715478c5aca719d14bb15b95c83c2f5d44545
jaydesl <jay.deslauriers@gmail.com>
2022-02-11 17:25:49 +0000

    refactor: handle tab and sheet names separately

--- tools/xls2md.py
+++ tools/xls2md.py
@@ -12,12 +12,9 @@ hide_edits = """
 """
 
 
-
-for sheet_name in sheets.keys():
-    sheet = pd.read_excel(
-        excel, sheet_name=sheet_name, skiprows=[0], usecols="A:G"
-    )
-    sheet_name = sheet_name.lower().replace(" ", "_")
+for tab in sheets.keys():
+    sheet = pd.read_excel(excel, sheet_name=tab, skiprows=[0], usecols="A:G")
+    sheet_name = tab.lower().replace(" ", "_")
     sheet.fillna("", inplace=True)
     table = sheet.to_markdown(index=False)
     with open(f"docs/tables/{sheet_name}.md", "w") as file:

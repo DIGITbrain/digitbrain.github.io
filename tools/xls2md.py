e73d27cf0289307921a532cd44b60a0160533d4c
jaydesl <jay.deslauriers@gmail.com>
2022-02-11 16:39:26 +0000

    feat(xls2md): use pandas built-in to_markdown()

--- tools/xls2md.py
+++ tools/xls2md.py
@@ -1,13 +1,8 @@
-import shutil
-import os
-
 import pandas as pd
-from mdtable import MDTable
 
 
 excel = "metadata.xlsx"
 sheets = pd.read_excel(excel, sheet_name=None)
-os.makedirs("csv", exist_ok=True)
 
 prepend = """
 <style>
@@ -22,16 +17,8 @@ for sheet_name in sheets.keys():
         excel, sheet_name=sheet_name, skiprows=[0], usecols="A:G"
     )
     sheet_name = sheet_name.lower().replace(" ", "_")
-    sheet.to_csv(f"csv/{sheet_name}.csv", index=False)
-    print(f"Generated {sheet_name}.csv...")
-    markdown = MDTable(f"csv/{sheet_name}.csv")
-    markdown_table = markdown.get_table()
-    markdown.save_table(f"docs/tables/{sheet_name}.md")
+    sheet.fillna("", inplace=True)
+    table = sheet.to_markdown(index=False)
+    with open(f"docs/tables/{sheet_name}.md", "w") as file:
+        file.write(prepend + "\n" + table)
     print(f"Converted to {sheet_name}.md...")
-    with open(f"docs/tables/{sheet_name}.md", "r+") as file:
-        content = file.read()
-        file.seek(0, 0)
-        file.write(prepend + "\n" + content)
-
-shutil.rmtree("csv")
-print(f"Cleaned up csv files...")
